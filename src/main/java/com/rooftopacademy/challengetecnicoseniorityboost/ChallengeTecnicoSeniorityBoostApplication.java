40246930300472df07506ba94de02ea095c3f74b
William Estrada <william.estrada@sofka.com.co>
2022-03-07 10:25:50 -0500

    base project

---
+++ src/main/java/com/rooftopacademy/challengetecnicoseniorityboost/ChallengeTecnicoSeniorityBoostApplication.java
@@ -0,0 +1,13 @@
+package com.rooftopacademy.challengetecnicoseniorityboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTecnicoSeniorityBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTecnicoSeniorityBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooftopacademy/challengetecnicoseniorityboost/ChallengeTecnicoSeniorityBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooftopacademy.challengetecnicoseniorityboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTecnicoSeniorityBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
